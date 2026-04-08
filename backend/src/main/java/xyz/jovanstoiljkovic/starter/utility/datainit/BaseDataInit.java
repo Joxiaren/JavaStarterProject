553b9887876faedc9244aa287bb6107f37c28f91
Shadow1993 <speedy93hl2dm@gmail.com>
2026-04-08 20:05:37 +0200

    Removed unused imports

--- backend/src/main/java/xyz/jovanstoiljkovic/starter/utility/datainit/BaseDataInit.java
+++ backend/src/main/java/xyz/jovanstoiljkovic/starter/utility/datainit/BaseDataInit.java
@@ -1,7 +1,5 @@
 package xyz.jovanstoiljkovic.starter.utility.datainit;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class BaseDataInit<T>{
     BaseDataGenerator<T> generator;
 
