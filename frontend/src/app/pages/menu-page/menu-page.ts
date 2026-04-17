c90148cde3249cfff086f7fa92292a007ed963ad
Joxiaren <2000jox@gmail.com>
2026-04-17 21:11:56 +0200

    refactor MenuPage clean imports and dead code

--- frontend/src/app/pages/menu-page/menu-page.ts
+++ frontend/src/app/pages/menu-page/menu-page.ts
@@ -1,8 +1,6 @@
 import { Component, inject, OnChanges, SimpleChanges } from '@angular/core';
 import { MenuForm } from 'app/components/menu-form/menu-form';
 import { MenuTable } from 'app/components/menu-table/menu-table';
-import { CategoryControl } from 'app/controls/category-control/category-control';
-import { FoodControl } from 'app/controls/food-control/food-control';
 import { MenuControl } from 'app/controls/menu-control/menu-control';
 
 @Component({
@@ -11,15 +9,11 @@ import { MenuControl } from 'app/controls/menu-control/menu-control';
   templateUrl: './menu-page.html',
   styleUrl: './menu-page.css',
 })
-export class MenuPage implements OnChanges{
+export class MenuPage {
   menuControl = inject(MenuControl);
 
   constructor(){
     this.dataRefresh();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
   }
 
   dataRefresh(){
