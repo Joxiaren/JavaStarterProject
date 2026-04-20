9784e6445e98ecfae29430377126e3679fb05e8e
Joxiaren <2000jox@gmail.com>
2026-04-20 14:26:35 +0200

    add eslint and prettierto frontend, remove .vscode

---
+++ frontend/eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = defineConfig([
+  {
+    files: ["**/*.spec.ts"],
+    rules: {}
+  },
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase"
+        }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case"
+        }
+      ]
+    }
+  },
+  {
+    files: ["**/*.html"],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {}
+  }
+]);
